962db304695572ac037a6c7abcd741fb654c5be8
xiaomayo233 <63353316+xiaomayo233@users.noreply.github.com>
2021-08-27 11:11:55 +0800

    Add files via upload

--- DiamondCalculator.js
+++ DiamondCalculator.js
@@ -2,15 +2,15 @@ function cut(i){
 	MAX_VALUE = BigInt("9223372036854775807");
 	MIN_VALUE = BigInt("-9223372036854775808");
 	while (i > MAX_VALUE) {
-		i = MIN_VALUE + (i - MAX_VALUE)
+		i = MIN_VALUE + (i - MAX_VALUE) - 1n
 	}
 	while (i < MIN_VALUE) {
-		i = MAX_VALUE - (MIN_VALUE - i);
+		i = MAX_VALUE - (MIN_VALUE - i) + 1n;
 	}
 	return i;
 }
 function cal(seed,cx,cz,v){
-	if (seed == null || cx == null || cz == null) {
+	if (seed == null || cx == null || cz == null && v == null) {
 		throw new Error("invalid data");
 	}
 	
@@ -21,16 +21,15 @@ function cal(seed,cx,cz,v){
 	var mul = BigInt("25214903917");
 	var mask = BigInt("281474976710655");
 	
-	var temp = seed ^ (mul & mask);
+	var temp = (seed ^ mul) & mask;
 
 	var first = cut((temp * mul + 11n) & mask);
-	var second = ((first * mul + 11n) & mask);
-	var third = ((second * mul + 11n) & mask);
-	var fourth = ((third * mul + 11n) & mask);
+	var second = cut((first * mul + 11n) & mask);
+	var third = cut((second * mul + 11n) & mask);
+	var fourth = cut((third * mul + 11n) & mask);
 	
 	/* 获取i j */
 	first >>= 16n;
-	console.log(first);
 	first <<= 32n;
 	first = cut(first);
 	
@@ -46,11 +45,11 @@ function cal(seed,cx,cz,v){
 	fourth = cut(fourth);
 	fourth >>= 32n;
 	
-	
-	console.log(first);
-
 	var i = cut(first + second) | 1n;
 	var j = cut(third + fourth) | 1n;
+	
+	console.log(i);
+	console.log(j);
 
 	/* 获取坐标 */
 	temp =((16n * cx * i + 16n * cz * j) ^ seed) + v;
